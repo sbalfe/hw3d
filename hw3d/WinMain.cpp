f94603dc8d60a68228068515a6bb3ee57cec88dc
Simon Balfe <sbmain17@gmail.com>
2021-08-09 22:32:54 +0100

    message-loop

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,13 +1,45 @@
 #include <Windows.h>
 
+
+/*
+	returns 32 bit integer values
+
+	must define calling convention of __stdcall
+
+	must pass in handle to window its processing the message of
+
+	must take the actual message structure
+
+	wparam and lparam are message dependent. 64 bit integers
+*/
+
+/*
+	meaning of result depends also on what you are handling 
+*/
 LRESULT CALLBACK WndProc( HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam )
 {
+	/*
+		switch case for dealing with each of the messages. 
+	*/
 	switch( msg )
 	{
 	case WM_CLOSE:
+		/*
+			indicate to the system that a thread has made a request to termiante
+
+			exit code define by us
+
+			returns wParam as the exit we can use to determine the exit code
+		*/
 		PostQuitMessage( 69 );
 		break;
 	}
+
+	/*
+		handle to window, msg structure and wide and long params.
+
+		this is called here to deal with any default behaviour that was not handled in our current logic.
+	*/
 
 	return DefWindowProc( hWnd,msg,wParam,lParam );
 }
@@ -21,9 +53,13 @@ int CALLBACK WinMain(
 	const auto pClassName = "hw3dbutts";
 	// register window class
 	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof( wc );
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
+
+	/*
+		example , does not close the window even after pressing X 
+		does not assume anything about number of windows to act on therefore must be defined
+	*/
+	wc.lpfnWndProc = WndProc; // Define our custom window procedure, away from the default one to be our open handler. 
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -46,14 +82,63 @@ int CALLBACK WinMain(
 	ShowWindow( hWnd,SW_SHOW );
 
 	// message pump
+
+	/*
+		tagMSG structure
+
+		hwnd > handle to current window
+		message > actual message identifier , low word only, high word for system only
+
+		wParam, lparam > context of the actual message, have different definitions for each message.
+
+		time > when the message was sent 
+
+		point > obtain the position of the cursor for every message that was received. 
+
+		lprivate > not used at all
+	*/
 	MSG msg;
 	BOOL gResult;
+	/*
+		GetMessage parameters
+		https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getmessage
+
+		lpmsg > pointer to msg structure that receives message info from the threads message queue
+
+		hWnd > pass in nullptr , this retrieves the message for any window belonging to the current thread
+			> both window messages and thread message are processed. 
+			> i.e get all messages of our .exe
+
+		filtermin , filter max > the range of message to pull of the queue 
+			> set both to 0 to process any message.
+
+				
+		return: if the message is not WM_QUIT then its non zero value, return 0 otherwise of course.
+			> -1 on error
+	*/
+
 	while( (gResult = GetMessage( &msg,nullptr,0,0 )) > 0 )
 	{
+		
+		/*
+			translates virtual key message into charcter messagees 
+			https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow
+
+			takes pointer to our MSG message struct, its a const and will generate auxiliary messages
+				that are passed to the queue again.
+		*/
 		TranslateMessage( &msg );
+
+		/*
+			pass message to the window proc related to this message 
+			https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-dispatchmessage
+		*/
 		DispatchMessage( &msg );
 	}
 
+	/*
+		return -1 if get message error has ocurred.
+	*/
 	if( gResult == -1 )
 	{
 		return -1;
