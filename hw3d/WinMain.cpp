f6f98d3762e5929249dc00cb98a33ea40860090b
Simon Balfe <sbmain17@gmail.com>
2021-08-09 18:12:14 +0100

    win-main

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,11 +1,21 @@
+/*
+	add all windows sdks from here 
+*/
 #include <Windows.h>
 
+
+/*
+	win32 entry point to the application , barebones windows application .exe
+*/
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR     lpCmdLine,
 	int       nCmdShow )
 {
+	/*
+	* run after windows instance created. 
+	*/
 	while( true );
 	return 0;
 }
